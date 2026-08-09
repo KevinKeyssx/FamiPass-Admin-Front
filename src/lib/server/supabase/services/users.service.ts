c1f706ca7a465aca3193c8844027401972a1aa65
kevinkeyssx <keyss1700@gmail.com>
2026-08-09 01:50:17 -0400

    users renamed to users.service

--- src/lib/server/supabase/services/users.service.ts
+++ src/lib/server/supabase/services/users.service.ts
@@ -1,4 +1,4 @@
-import { supabaseServer } from './supabase.js';
+import { supabaseServer } from '../supabase.js';
 
 /**
  * Obtiene el rol de un usuario desde la base de datos de Supabase dado su correo electrónico.
@@ -6,14 +6,14 @@ import { supabaseServer } from './supabase.js';
  * @param email Correo electrónico del usuario.
  * @returns Promesa que se resuelve con el rol del usuario ('ADMIN' | 'STAFF' | 'MEMBER') o null si no se encuentra.
  */
-export async function getUserRole( email: string ): Promise<string | null> {
-	console.log('🚀 ~ getUserRole ~ email:', email)
+export async function getUserRole(
+    email: string
+): Promise<string | null> {
 	const { data } = await supabaseServer
 		.from( 'users' )
 		.select( 'role' )
 		.eq( 'email', email )
 		.single();
-	console.log('🚀 ~ getUserRole ~ data:', data)
 
 	return data?.role ?? null;
 }
