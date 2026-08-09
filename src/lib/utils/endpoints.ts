39698226672de35ac0ccd43c7deb1a9b7aa415d4
kevinkeyssx <keyss1700@gmail.com>
2026-08-09 01:50:52 -0400

    not used updated

--- src/lib/utils/endpoints.ts
+++ src/lib/utils/endpoints.ts
@@ -1,9 +1,1 @@
-export const INTERNAL_ENDPOINT = {
-    events: {
-        list  : '/api/events',
-        create: '/api/events',
-        detail: ( id: string ): string => `/api/events/${ id }`,
-        update: ( id: string ): string => `/api/events/${ id }`,
-        delete: ( id: string ): string => `/api/events/${ id }`,
-    },
-};
+export const INTERNAL_ENDPOINT = {};
