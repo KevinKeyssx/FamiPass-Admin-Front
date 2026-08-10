2ec15f34fb2dae262dd3a0d2ebbc57938e99b9f2
kevinkeyssx <keyss1700@gmail.com>
2026-08-10 00:40:48 -0400

    depends & filters implemented

--- src/routes/(admin)/events/+page.server.ts
+++ src/routes/(admin)/events/+page.server.ts
@@ -2,15 +2,44 @@ import { fail } from '@sveltejs/kit';
 
 import type { PageServerLoad, Actions } from './$types.js';
 import { getEvents, deleteEvent }       from '$lib/server/supabase/services/events.service.js';
+import type { EventStatus }             from '$lib/types/index.js';
 
 
-export const load: PageServerLoad = async () => {
-	const events = await getEvents();
+export const load: PageServerLoad = async ( { url, depends } ) => {
+	depends( 'app:events' );
 
-	return {
-		events
-	};
+	const search       = url.searchParams.get( 'search' ) || '';
+	const date         = url.searchParams.get( 'date' ) || '';
+	const status       = ( url.searchParams.get( 'status' ) || 'ALL' ) as 'ALL' | EventStatus;
+	const verification = ( url.searchParams.get( 'verification' ) || 'ALL' ) as 'ALL' | 'TRUE' | 'FALSE';
+	const minors       = ( url.searchParams.get( 'minors' ) || 'ALL' ) as 'ALL' | 'TRUE' | 'FALSE';
+	const page         = Number( url.searchParams.get( 'page' ) || '1' );
+	const pageSize     = Number( url.searchParams.get( 'pageSize' ) || '12' );
+
+	try {
+		const { data: events, count } = await getEvents( {
+			search,
+			date,
+			status,
+			verification,
+			minors,
+			page,
+			pageSize
+		});
+
+		return {
+			events,
+			count
+		};
+	} catch ( err: any ) {
+		return {
+			events : [],
+			count  : 0,
+			error  : err.message
+		};
+	}
 };
+
 
 export const actions: Actions = {
 	delete: async ( { request } ) => {
@@ -23,7 +52,8 @@ export const actions: Actions = {
 
 		try {
 			await deleteEvent( id );
-			return { success: true };
+
+            return { success: true };
 		} catch ( err: any ) {
 			return fail( 400, { error: err.message } );
 		}
