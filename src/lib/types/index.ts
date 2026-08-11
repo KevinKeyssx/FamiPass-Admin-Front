a8e2284b950888c5516414d3b6465accc962405f
kevinkeyssx <keyss1700@gmail.com>
2026-08-11 00:39:49 -0400

    familyMember implemented

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,5 +1,6 @@
-export type { EventStatus, EventConfig }                                     from './event.js';
-export type { CommunityOrganization, RelationshipType, UserRole, User }      from './user.js';
-export type { Family, FamilyEvent }                                          from './family.js';
-export type { Product }                                                      from './product.js';
-export type { OrderStatus, FamilyMemberSnapshot, OrderItem, Order }         from './order.js';
+export type { EventStatus, EventConfig }                                from './event.js';
+export type { UserRole, User }                                          from './user.js';
+export type { Family, FamilyEvent }                                     from './family.js';
+export type { Product }                                                 from './product.js';
+export type { OrderStatus, FamilyMemberSnapshot, OrderItem, Order }     from './order.js';
+export type { CommunityOrganization, RelationshipType, FamilyMember }   from './familyMember.js';
