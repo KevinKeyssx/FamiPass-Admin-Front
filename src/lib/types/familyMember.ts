eff50bc8b877495e2f1fefac344add684a94de31
kevinkeyssx <keyss1700@gmail.com>
2026-09-10 23:55:43 -0300

    user_id & role implemented

--- src/lib/types/familyMember.ts
+++ src/lib/types/familyMember.ts
@@ -1,24 +1,30 @@
-import type { Family } from "./family";
+import type { Family } from './family.js';
+import type { User }   from './user.js';
 
 export type CommunityOrganization =
-    | 'CUORUM'
-    | 'SOC_SOC'
-    | 'HOMBRES_JOVENES'
-    | 'MUJERES_JOVENES'
-    | 'PRIMARIA'
-    | 'FRIENDS'
-    | 'NINGUNA';
+	| 'CUORUM'
+	| 'SOC_SOC'
+	| 'HOMBRES_JOVENES'
+	| 'MUJERES_JOVENES'
+	| 'PRIMARIA'
+	| 'FRIENDS'
+	| 'NINGUNA';
 
+export type FamilyMemberRole = 'VIEWER' | 'AGGREGATOR' | 'ADMIN';
 
 export interface FamilyMember {
-    id                  : string;
-    family_id           : string;
-    full_name           : string;
-    rut                 : string;
-    phone?              : string | null;
-    organization        : CommunityOrganization;
-    is_representative   : boolean;
-    created_at?         : string;
-    updated_at?         : string;
-    family?             : Family;
+	id					: string;
+	family_id			: string;
+	user_id?			: string | null;
+	full_name			: string;
+	rut					: string;
+	email?				: string | null;
+	phone?				: string | null;
+	organization		: CommunityOrganization;
+	is_representative	: boolean;
+	role				: FamilyMemberRole;
+	created_at?			: string;
+	updated_at?			: string;
+	family?				: Family;
+	user?				: User;
 }
