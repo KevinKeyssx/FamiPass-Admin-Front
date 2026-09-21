f6b9882017890c74fb298f1537ba73dc1c24a906
kevinkeyssx <keyss1700@gmail.com>
2026-09-21 02:24:55 -0300

    rut is optional

--- src/lib/types/familyMember.ts
+++ src/lib/types/familyMember.ts
@@ -13,18 +13,18 @@ export type CommunityOrganization =
 export type FamilyMemberRole = 'VIEWER' | 'AGGREGATOR' | 'ADMIN';
 
 export interface FamilyMember {
-	id					: string;
-	family_id			: string;
-	user_id?			: string | null;
-	full_name			: string;
-	rut					: string;
-	email?				: string | null;
-	phone?				: string | null;
-	organization		: CommunityOrganization;
-	is_representative	: boolean;
-	role				: FamilyMemberRole;
-	created_at?			: string;
-	updated_at?			: string;
-	family?				: Family;
-	user?				: User;
+	id                : string;
+	family_id         : string;
+	user_id?          : string | null;
+	full_name         : string;
+	rut?              : string | null;
+	email?            : string | null;
+	phone?            : string | null;
+	organization      : CommunityOrganization;
+	is_representative : boolean;
+	role              : FamilyMemberRole;
+	created_at?       : string;
+	updated_at?       : string;
+	family?           : Family;
+	user?             : User;
 }
